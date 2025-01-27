7a16e1518a455c336960f5b9d8d0e57847306f42
uma <uma@tigeen.ch>
2025-01-27 18:06:27 +0530

    gemini api integrtaedd

--- app/hooks/useAIChat.jsx
+++ app/hooks/useAIChat.jsx
@@ -3,7 +3,49 @@ import { useState } from "react";
 const useAiChat = () => {
   const [state, setState] = useState({
     messages: [],
+    currentQuestion: "",
   });
-  return {};
+  const getAnswer = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        question: state.currentQuestion,
+      }),
+    };
+    const response = await fetch(
+      "http://192.168.188.73:5000/api/gemini/gemini-request",
+      requestOptions
+    );
+
+    const result = await response.json();
+
+    if (result.status) {
+      setState((prev) => ({
+        ...prev,
+        messages: [
+          ...prev.messages,
+          {
+            question: state.currentQuestion,
+            answer: result.data,
+          },
+        ],
+        currentQuestion: "",
+      }));
+    }
+  };
+  const inputQuestion = (text = "") => {
+    setState((prev) => ({
+      ...prev,
+      currentQuestion: text,
+    }));
+  };
+  return {
+    ...state,
+    inputQuestion,
+    getAnswer,
+  };
 };
 export default useAiChat;
--- app/screens/chat/index.jsx
+++ app/screens/chat/index.jsx
@@ -1,17 +1,37 @@
-import { TextInput, View } from "react-native";
+import { FlatList, Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import useAIChat from "../../hooks/useAIChat";
+
 const AIChat = () => {
+  const { messages, inputQuestion, getAnswer } = useAIChat();
+  const QandACard = ({ item }) => {
+    console.log(item);
+
+    return (
+      <View>
+        <Text>{item.question}</Text>
+        <Text>{item.answer}</Text>
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
+      <FlatList data={messages} renderItem={QandACard} />
       <View style={styles.questionInputSectionContainer}>
         <TextInput
           style={styles.textInputStyle}
+          onChangeText={inputQuestion}
           placeholder="Ask Gemini"
           multiline
           autoFocus
         />
-        <MaterialIcons name="send" size={24} color="black" />
+        <MaterialIcons
+          name="send"
+          size={24}
+          color="black"
+          onPress={getAnswer}
+        />
       </View>
     </View>
   );

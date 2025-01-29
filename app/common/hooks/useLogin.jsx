d795f2500da21b2561a5c42f64cdf5d29984cf25
uma <uma@tigeen.ch>
2025-01-29 13:14:05 +0530

    new hook for login added

--- app/common/hooks/useLogin.jsx
+++ app/common/hooks/useLogin.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import Constants from "expo-constants";
 
-const useAiChat = () => {
+const useLogin = () => {
   const [state, setState] = useState({
     isLoading: false,
   });
-  const { API_URL } = Constants.manifest2.extra;
+  const { API_URL } = Constants.expoConfig.extra;
   const authenticateUser = async () => {
     setState((prev) => ({
       ...prev,
@@ -22,22 +22,11 @@ const useAiChat = () => {
       },
       body: JSON.stringify(data),
     };
-    const response = await fetch(
-      `${process.env.BASE_API_URL}/gemini/gemini-request`,
-      requestOptions
-    );
+    const response = await fetch(`${API_URL}/auth/login`, requestOptions);
 
     const result = await response.json();
 
     if (result.status) {
-      setState((prev) => {
-        prev.messages.push({
-          data: result.data,
-          sender: "gemini",
-        });
-
-        return { ...prev };
-      });
     }
     setState((prev) => ({
       ...prev,
@@ -51,4 +40,4 @@ const useAiChat = () => {
     authenticateUser,
   };
 };
-export default useAiChat;
+export default useLogin;

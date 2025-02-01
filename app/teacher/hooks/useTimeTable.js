c7ca8c616b16595e96aee3ef072108291e574277
uma <uma@tigeen.ch>
2025-02-01 14:54:16 +0530

    mock data added for display test

--- app/teacher/hooks/useTimeTable.js
+++ app/teacher/hooks/useTimeTable.js
@@ -3,8 +3,25 @@ import { useState } from "react";
 const useTimeTable = () => {
   const [state, setState] = useState({
     class: null,
-    workDays: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    workDays: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "saturday",
+      "sunday",
+    ],
     addPeriodModalVisibility: false,
+    timetable: {
+      monday: {
+        assembly: "9:00 am  - 10:00 am",
+        maths: "10:00 am - 10:30 am",
+      },
+      tuesday: {
+        assembly: "9:00 am  - 10:00 am",
+      },
+    },
   });
   const hideModal = () =>
     setState((prev) => ({ ...prev, addPeriodModalVisibility: false }));

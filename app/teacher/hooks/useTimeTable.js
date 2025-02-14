70b69674e3c785ed6a1d38ad40b711d2faf95048
uma <uma@tigeen.ch>
2025-02-14 15:56:31 +0530

    publish timetable function added

---
+++ app/teacher/hooks/useTimeTable.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Constants from "expo-constants";
+const useTimeTable = () => {
+  const [state, setState] = useState({
+    isLoading: false,
+  });
+  const { API_URL } = Constants.expoConfig.extra;
+
+  const publishTimeTable = async () => {
+    try {
+      setState((prev) => ({ ...prev, isLoading: true }));
+      const data = {};
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      const url = `${API_URL}`;
+      const response = await fetch(url, requestOptions);
+      const result = await response.json();
+      setState((prev) => ({ ...prev, isLoading: false }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return {
+    ...state,
+    setState,
+    publishTimeTable,
+  };
+};
+export default useTimeTable;

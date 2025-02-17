7c0249789c2ff51c3164d369b848195a30f82f3c
uma <uma@tigeen.ch>
2025-02-17 19:27:24 +0530

    validation added

--- app/teacher/components/PeriodInfoEdit/index.jsx
+++ app/teacher/components/PeriodInfoEdit/index.jsx
@@ -5,81 +5,19 @@ import { ScrollView, Text, TextInput, View } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { useState } from "react";
 import { useTeacherContext } from "../../context/useTeacherContext";
+import usePeriodModal from "../../hooks/usePeriodModal";
 
 const PeriodInfoEdit = ({ hideModal = () => {}, day = "" }) => {
-  const [subject, setSubject] = useState("");
-  const hoursOptions = [
-    {
-      label: "01",
-      value: "01",
-    },
-    {
-      label: "02",
-      value: "02",
-    },
-    {
-      label: "03",
-      value: "03",
-    },
-    {
-      label: "04",
-      value: "04",
-    },
-    {
-      label: "05",
-      value: "05",
-    },
-    {
-      label: "06",
-      value: "06",
-    },
-    {
-      label: "07",
-      value: "07",
-    },
-    {
-      label: "08",
-      value: "08",
-    },
-    {
-      label: "09",
-      value: "09",
-    },
-    {
-      label: "10",
-      value: "10",
-    },
-    {
-      label: "11",
-      value: "11",
-    },
-    {
-      label: "12",
-      value: "12",
-    },
-  ];
-  const timemeridiam = [
-    {
-      label: "am",
-      value: "am",
-    },
-    {
-      label: "pm",
-      value: "pm",
-    },
-  ];
-  const minuteCreationFunction = () => {
-    const minutesArray = [];
-    for (let i = 0; i < 60; i++) {
-      minutesArray.push({
-        value: i < 10 ? `0${i}` : `${i}`,
-        label: i < 10 ? `0${i}` : `${i}`, // Formatting minutes as "01", "02", ..., "59"
-      });
-    }
-    return minutesArray;
-  };
-  const [startTime, setStartTime] = useState({ hr: "", min: "", meridiam: "" });
-  const [endTime, setEndTime] = useState({ hr: "", min: "", meridiam: "" });
+  const {
+    subjectError,
+    startTimeError,
+    endTimeError,
+    hoursOptions,
+    timemeridiam,
+    setState,
+    addValidPeriodDataToRespectiveDay,
+    minuteCreationFunction,
+  } = usePeriodModal();
   const { addPeriodOfDay } = useTeacherContext();
   return (
     <View style={styles.container}>
@@ -94,7 +32,9 @@ const PeriodInfoEdit = ({ hideModal = () => {}, day = "" }) => {
         <TextInput
           style={styles.textInputStyle}
           placeholder="Period Name  (ex.)Assembly"
-          onChangeText={(text) => setSubject(text)}
+          onChangeText={(text) =>
+            setState((prev) => ({ ...prev, subject: text }))
+          }
         />
         <Text style={styles.ORTextStyle}>OR</Text>
         <DropDown
@@ -102,20 +42,29 @@ const PeriodInfoEdit = ({ hideModal = () => {}, day = "" }) => {
           placeHolder="Course"
           placeHolderStyle={styles.dropdownPlaceHolderStyle}
         />
+        <Text>{subjectError}</Text>
         <Text style={styles.inputTitleTextStyle}>Start Time</Text>
         <View style={styles.displayStack1}>
           <DropDown
             style={styles.timeInput}
             data={hoursOptions}
             placeHolder="hours"
-            onChange={(text) => setStartTime((prev) => ({ ...prev, hr: text }))}
+            onChange={(text) =>
+              setState((prev) => ({
+                ...prev,
+                startTime: { ...prev.startTime, hr: text },
+              }))
+            }
           />
           <DropDown
             style={styles.timeInput}
             placeHolder="mins"
             data={minuteCreationFunction()}
             onChange={(text) =>
-              setStartTime((prev) => ({ ...prev, min: text }))
+              setState((prev) => ({
+                ...prev,
+                startTime: { ...prev.startTime, min: text },
+              }))
             }
           />
           <DropDown
@@ -123,45 +72,59 @@ const PeriodInfoEdit = ({ hideModal = () => {}, day = "" }) => {
             data={timemeridiam}
             placeHolder="am/pm"
             onChange={(text) =>
-              setStartTime((prev) => ({
+              setState((prev) => ({
                 ...prev,
-                meridiam: text,
+                startTime: {
+                  ...prev.startTime,
+                  meridiam: text,
+                },
               }))
             }
           />
         </View>
+
         <Text style={styles.inputTitleTextStyle}>End Time</Text>
         <View style={styles.displayStack1}>
           <DropDown
             style={styles.timeInput}
             placeHolder="hours"
             data={hoursOptions}
-            onChange={(text) => setEndTime((prev) => ({ ...prev, hr: text }))}
+            onChange={(text) =>
+              setState((prev) => ({
+                ...prev,
+                endTime: { ...prev.endTime, hr: text },
+              }))
+            }
           />
           <DropDown
             style={styles.timeInput}
             placeHolder="mins"
             data={minuteCreationFunction()}
-            onChange={(text) => setEndTime((prev) => ({ ...prev, min: text }))}
+            onChange={(text) =>
+              setState((prev) => ({
+                ...prev,
+                endTime: { ...prev.endTime, min: text },
+              }))
+            }
           />
           <DropDown
             style={styles.timeInput}
             placeHolder="am/pm"
             data={timemeridiam}
             onChange={(text) =>
-              setEndTime((prev) => ({ ...prev, meridiam: text }))
+              setState((prev) => ({
+                ...prev,
+                endTime: { ...prev.endTime, meridiam: text },
+              }))
             }
           />
         </View>
         <Pressable
           style={styles.saveButtonStyle}
           onPress={() => {
-            addPeriodOfDay({
+            addValidPeriodDataToRespectiveDay({
               day,
-              payload: {
-                time: `${startTime.hr}:${startTime.min} ${startTime.meridiam}-${endTime.hr}:${endTime.min} ${endTime.meridiam}`,
-                subject,
-              },
+              callback: addPeriodOfDay,
             });
           }}
         >
--- app/teacher/hooks/usePeriodModal.js
+++ app/teacher/hooks/usePeriodModal.js
@@ -2,16 +2,130 @@ import { useState } from "react";
 
 const usePeriodModal = () => {
   const [state, setState] = useState({
+    isSaving: false,
     addPeriodModalVisibility: false,
+    subject: "",
+    startTime: { hr: "", min: "", meridiam: "" },
+    endTime: { hr: "", min: "", meridiam: "" },
+    subjectError: "",
+    startTimeError: "",
+    endTimeError: "",
   });
+
+  const hoursOptions = [
+    {
+      label: "01",
+      value: "01",
+    },
+    {
+      label: "02",
+      value: "02",
+    },
+    {
+      label: "03",
+      value: "03",
+    },
+    {
+      label: "04",
+      value: "04",
+    },
+    {
+      label: "05",
+      value: "05",
+    },
+    {
+      label: "06",
+      value: "06",
+    },
+    {
+      label: "07",
+      value: "07",
+    },
+    {
+      label: "08",
+      value: "08",
+    },
+    {
+      label: "09",
+      value: "09",
+    },
+    {
+      label: "10",
+      value: "10",
+    },
+    {
+      label: "11",
+      value: "11",
+    },
+    {
+      label: "12",
+      value: "12",
+    },
+  ];
+  const timemeridiam = [
+    {
+      label: "am",
+      value: "am",
+    },
+    {
+      label: "pm",
+      value: "pm",
+    },
+  ];
+
   const hideModal = () =>
     setState((prev) => ({ ...prev, addPeriodModalVisibility: false }));
   const showModal = () =>
     setState((prev) => ({ ...prev, addPeriodModalVisibility: true }));
 
+  // function creates option for dropdown for minutes
+  const minuteCreationFunction = () => {
+    const minutesArray = [];
+    for (let i = 0; i < 60; i++) {
+      minutesArray.push({
+        value: i < 10 ? `0${i}` : `${i}`,
+        label: i < 10 ? `0${i}` : `${i}`, // Formatting minutes as "01", "02", ..., "59"
+      });
+    }
+    return minutesArray;
+  };
+  const validationErrorMessage = () => {
+    setState((prev) => {
+      return {
+        ...prev,
+        subjectError: prev.subject ? "" : "Enter subject",
+        startTimeError: prev.startTime ? "" : "Enter start Time",
+        endTimeError: prev.endTime ? "" : "Enter start Time",
+      };
+    });
+  };
+  const addValidPeriodDataToRespectiveDay = ({ day, callback }) => {
+    if (
+      state.subject &&
+      state.startTime.hr &&
+      state.startTime.meridiam &&
+      state.startTime.min &&
+      state.endTime.hr &&
+      state.endTime.meridiam &&
+      state.endTime.min
+    ) {
+      callback({
+        day,
+        payload: {
+          time: `${state.startTime.hr}:${state.startTime.min} ${state.startTime.meridiam}-${state.endTime.hr}:${state.endTime.min} ${state.endTime.meridiam}`,
+          subject: state.subject,
+        },
+      });
+    } else validationErrorMessage();
+  };
   return {
     ...state,
+    hoursOptions,
+    timemeridiam,
     setState,
+
+    addValidPeriodDataToRespectiveDay,
+    minuteCreationFunction,
     hideModal,
     showModal,
   };

cc77b8a0a89c28f4437d205377c86ffc4b9c8b2d
uma <uma@tigeen.ch>
2025-02-21 06:07:49 +0530

    heigth added

--- app/teacher/components/BasicInfo/index.jsx
+++ app/teacher/components/BasicInfo/index.jsx
@@ -1,11 +1,19 @@
 import { styles } from "./styles";
 import DropDown from "@/app/common/components/DropDown";
+import { Constants } from "@/app/common/constants";
+import moderateScale from "@/app/utils/responsiveScale";
 import { Pressable, Text, TextInput, View } from "react-native";
 const BasicInfo = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.headingTextStyle}>Basic Info</Text>
-      <DropDown style={styles.textInputStyle} placeHolder="Class ID" />
+      <DropDown
+        style={styles.textInputStyle}
+        placeHolder="Class ID"
+        data={Constants.common.classIds}
+        labelField="value"
+        containerStyle={{ height: moderateScale(250) }}
+      />
       <TextInput style={styles.textInputStyle} placeholder="Mentor Name" />
       <Pressable style={styles.actionableButtonStyle}>
         <Text style={styles.actionableButtonTextStyle}>Save</Text>

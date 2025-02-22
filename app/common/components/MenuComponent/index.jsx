dbe9773f08375a596eed9ef27ffb9d643554c415
uma <uma@tigeen.ch>
2025-02-23 00:17:57 +0530

    menu for all period

--- app/common/components/MenuComponent/index.jsx
+++ app/common/components/MenuComponent/index.jsx
@@ -1,12 +1,17 @@
 import useLogout from "@/app/common/hooks/useLogout";
 import { styles } from "./styles";
-
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
 import { Pressable, Text, View } from "react-native";
 const MenuComponent = () => {
   const { logoutUser } = useLogout();
   return (
     <View style={styles.container}>
+      <Pressable style={styles.displayStack1}>
+        <FontAwesome6 name="table-list" size={24} color="black" />
+        <Text style={styles.itemTextStyle}>View All</Text>
+      </Pressable>
       <Pressable style={styles.displayStack1} onPress={logoutUser}>
         <MaterialIcons
           name="account-circle"

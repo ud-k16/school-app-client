040897387f7ebad73f01af1ee0a1d0f1e1239ee9
uma <uma@tigeen.ch>
2025-02-25 03:07:49 +0530

    class card added

--- src/teacher/components/TimeTableCard/index.jsx
+++ src/teacher/components/TimeTableCard/index.jsx
@@ -3,21 +3,36 @@ import { styles } from "./styles";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { Pressable, Text, View } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Themes } from "@/src/utils/themes";
 const TimeTableCard = ({}) => {
   const { user } = useAuthContext();
   return (
     <View style={styles.container} onPress={() => {}}>
-      <MaterialCommunityIcons
-        name="file-document-edit-outline"
-        size={24}
-        color="black"
-      />
-      <MaterialIcons name="table-view" size={24} color="black" />
-      <Text>{user.classId ?? "CLASS6A"}</Text>
-      <Text>{user.classId ?? "Published"}</Text>
-      <View>
-        <Text>View</Text>
-        <Text>Publish</Text>
+      <View style={styles.classIdContainer}>
+        <Text style={styles.classIdTextStyle}>{user.classId ?? "CLASS6A"}</Text>
+      </View>
+      <View style={styles.statusContainer}>
+        <Text style={styles.statusTextStyle}>{"Status : "}</Text>
+        <Text
+          style={[styles.statusTextStyle, { color: Themes.lightFadedGreen }]}
+        >
+          {"Published"}
+        </Text>
+      </View>
+
+      <View style={styles.displayStack1}>
+        <Pressable style={styles.displayStack2}>
+          <MaterialCommunityIcons
+            name="file-document-edit-outline"
+            size={24}
+            color="black"
+          />
+          <Text>Edit</Text>
+        </Pressable>
+        <Pressable style={styles.displayStack2}>
+          <MaterialIcons name="table-view" size={24} color="black" />
+          <Text>Publish</Text>
+        </Pressable>
       </View>
     </View>
   );
--- src/teacher/components/TimeTableCard/styles.js
+++ src/teacher/components/TimeTableCard/styles.js
@@ -10,8 +10,34 @@ export const styles = StyleSheet.create({
     borderRadius: moderateScale(5),
     elevation: 5,
     backgroundColor: Themes.white,
+    rowGap: moderateScale(20),
   },
   classIdContainer: {
-    height: moderateScale(60),
+    height: moderateScale(70),
+    backgroundColor: Themes.lightFadedGreen,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  classIdTextStyle: {
+    fontSize: moderateScale(20),
+    color: Themes.white,
+  },
+  statusContainer: {
+    flexDirection: "row",
+    alignSelf: "flex-end",
+    paddingRight: moderateScale(15),
+    alignItems: "center",
+  },
+  statusTextStyle: {
+    fontWeight: 600,
+  },
+  displayStack1: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  displayStack2: {
+    justifyContent: "center",
+    alignItems: "center",
   },
 });

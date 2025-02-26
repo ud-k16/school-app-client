70629895fd04a19f42122a85c8b2a5158fd0e96a
uma <uma@tigeen.ch>
2025-02-26 22:05:24 +0530

    chat screen navigation addede

--- src/common/components/MenuComponent/index.jsx
+++ src/common/components/MenuComponent/index.jsx
@@ -2,6 +2,8 @@ import useLogout from "@/src/common/hooks/useLogout";
 import { styles } from "./styles";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Pressable, Text, View } from "react-native";
 import { router } from "expo-router";
 import { useAuthContext } from "../../context/useAuthContext";
@@ -19,7 +21,12 @@ const MenuComponent = () => {
             });
           }}
         >
-          <FontAwesome6 name="table-list" size={24} color="black" />
+          <AntDesign
+            name="calendar"
+            size={24}
+            color="black"
+            style={styles.iconStyle}
+          />
           <Text style={styles.itemTextStyle}>Schedule</Text>
         </Pressable>
       )}
@@ -32,10 +39,31 @@ const MenuComponent = () => {
             });
           }}
         >
-          <MaterialIcons name="subject" size={24} color="black" />
+          <MaterialIcons
+            name="subject"
+            size={24}
+            color="black"
+            style={styles.iconStyle}
+          />
           <Text style={styles.itemTextStyle}>Course</Text>
         </Pressable>
       )}
+      <Pressable
+        style={styles.displayStack1}
+        onPress={() => {
+          router.navigate({
+            pathname: "/common/chat",
+          });
+        }}
+      >
+        <Ionicons
+          name="chatbox-outline"
+          size={24}
+          color="black"
+          style={styles.iconStyle}
+        />
+        <Text style={styles.itemTextStyle}>Gemini AI </Text>
+      </Pressable>
       <Pressable style={styles.displayStack1} onPress={logoutUser}>
         <MaterialIcons
           name="account-circle"

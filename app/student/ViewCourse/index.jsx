96bb91eb6297e8631ac3e5bcfa377e465a93ddca
uma <uma@tigeen.ch>
2025-03-01 12:04:04 +0530

    course card added to student

--- app/student/ViewCourse/index.jsx
+++ app/student/ViewCourse/index.jsx
@@ -1,21 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
-import Header from "@/src/common/components/Header";
-import CourseCard from "@/src/common/components/CourseCard";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { useStudentContext } from "@/src/student/context/useStudentContext";
+import CourseCard from "../../../src/teacher/components/CourseCard";
+import Header from "../../../src/common/components/Header";
+import moderateScale from "@/src/utils/responsiveScale";
+
 const ViewCourse = () => {
-  const { course } = useStudentContext();
+  const { course, isLoading } = useStudentContext();
+
+  if (isLoading)
+    return (
+      <View>
+        <Text>Loading ...</Text>
+      </View>
+    );
   return (
     <View style={styles.container}>
-      <Header title={"Course"} />
-      {course?.map((course, index) => (
-        <CourseCard key={index} course={course} />
-      ))}
+      <Header title={"Courses"} />
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+        <View>
+          {course?.map((coursedata, index) => {
+            return (
+              <CourseCard
+                key={index}
+                index={index}
+                subject={coursedata.subject}
+                description={coursedata.description}
+                teacher={coursedata.teacher}
+              />
+            );
+          })}
+        </View>
+      </ScrollView>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container: { flex: 1, rowGap: moderateScale(5) },
+  scrollViewContainer: {
+    flexGrow: 1,
+    rowGap: moderateScale(10),
   },
 });
 export default ViewCourse;

e91ffabfcc3770e3ba891dd0b41a166e0885b46e
uma <uma@tigeen.ch>
2025-03-03 20:35:53 +0530

    publish course function added

--- src/teacher/hooks/useCourse.js
+++ src/teacher/hooks/useCourse.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { useTeacherContext } from "@/src/teacher/context/useTeacherContext";
+import { useAuthContext } from "@/src/common/context/useAuthContext";
+import { fetchWithTimeOut } from "@/src/utils/helperFunctions";
+
 const useCourse = () => {
-  const [state, setState] = useState();
-  const { setState: setTeacherContext } = useTeacherContext();
+  // const [state, setState] = useState();
+  const { user } = useAuthContext();
+  const { setState: setTeacherContext, course } = useTeacherContext();
   const editCourse = ({ index, subject, teacher, description }) => {
     setTeacherContext((prev) => {
       prev.course[index] = {
@@ -31,12 +35,38 @@ const useCourse = () => {
       return { ...prev };
     });
   };
+
+  const publishCourse = async () => {
+    try {
+      setTeacherContext((prev) => ({ ...prev, isLoading: true }));
+
+      const data = { id: user.classId, course };
+      console.log("sending course data to server : ", data);
+      // request option object
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      const url = `${API_URL}/course/publish`;
+      const response = await fetchWithTimeOut(url, requestOptions);
+      const result = await response.json();
+      console.log("Response from server on publish of course", result);
+      // set loading indicator false
+      setState((prev) => ({ ...prev, isLoading: false }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return {
     ...state,
     setState,
     insertCourse,
     editCourse,
     deleteCourse,
+    publishCourse,
   };
 };
 export default useCourse;

f5fd7da83352296a9c7a23fa88e23b76d152db38
uma <uma@tigeen.ch>
2025-03-05 10:40:58 +0530

    edit time table style file removed

--- app/teacher/EditTimeTable/index.jsx
+++ app/teacher/EditTimeTable/index.jsx
@@ -1,8 +1,10 @@
-import { ScrollView, View } from "react-native";
-import { styles } from "./styles";
+import { ScrollView, View, StyleSheet } from "react-native";
 import BasicInfo from "@/src/teacher/components/BasicInfo";
 import WorkDays from "@/src/teacher/components/WorkDays";
 import Header from "@/src/common/components/Header";
+import moderateScale from "@/src/utils/responsiveScale";
+import { Themes } from "@/src/utils/themes";
+
 const EditTimeTable = () => {
   return (
     <View style={styles.container}>
@@ -14,4 +16,35 @@ const EditTimeTable = () => {
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  scrollViewContainer: {
+    flexGrow: 1,
+    rowGap: moderateScale(10),
+  },
+  textInputStyle: {
+    alignSelf: "center",
+    width: "90%",
+    height: moderateScale(60),
+    borderWidth: moderateScale(2),
+    borderRadius: moderateScale(5),
+    paddingHorizontal: moderateScale(10),
+  },
+  actionableButtonStyle: {
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Themes.secondary,
+    width: "60%",
+    height: moderateScale(50),
+    borderRadius: moderateScale(5),
+  },
+  actionableButtonTextStyle: {
+    color: Themes.white,
+  },
+});
+
 export default EditTimeTable;

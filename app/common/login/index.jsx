1b4d045ef74548a45c1d1080cbb5e30df348d045
uma <uma@tigeen.ch>
2025-03-05 10:35:45 +0530

    style file removed

--- app/common/login/index.jsx
+++ app/common/login/index.jsx
@@ -1,7 +1,17 @@
-import { styles } from "./styles";
 import LOGO from "@/src/assets/logo.png";
-import { Image, Pressable, Text, TextInput, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import useLogin from "@/src/common/hooks/useLogin";
+const { Themes } = require("../../../src/utils/themes");
+const {
+  default: moderateScale,
+} = require("../../../src/utils/responsiveScale");
 
 const Login = () => {
   const { setState, authenticateUser } = useLogin();
@@ -27,4 +37,40 @@ const Login = () => {
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: moderateScale(10),
+    rowGap: moderateScale(20),
+  },
+  logoConatiner: {
+    alignSelf: "center",
+    height: moderateScale(200),
+    width: "90%",
+    resizeMode: "contain",
+  },
+  textInputStyle: {
+    alignSelf: "center",
+    width: "90%",
+    height: moderateScale(60),
+    borderColor: Themes.greyShade,
+    borderWidth: moderateScale(2),
+    borderStyle: "solid",
+    borderRadius: moderateScale(5),
+    paddingHorizontal: moderateScale(10),
+  },
+  loginButtonStyle: {
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Themes.secondary,
+    width: "60%",
+    height: moderateScale(50),
+    borderRadius: moderateScale(5),
+  },
+  loginButtonTextStyle: {
+    color: Themes.white,
+  },
+});
+
 export default Login;

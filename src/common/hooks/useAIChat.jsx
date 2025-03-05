2455b65a42cee41f3fcc909b9b6deedd716ca537
uma <uma@tigeen.ch>
2025-03-05 22:47:20 +0530

    fetchtime out function in chat

--- src/common/hooks/useAIChat.jsx
+++ src/common/hooks/useAIChat.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import Constants from "expo-constants";
+import { fetchWithTimeOut } from "@/src/utils/helperFunctions";
 
 const useAiChat = () => {
   const [state, setState] = useState({
@@ -11,41 +12,48 @@ const useAiChat = () => {
   const { API_URL } = Constants.expoConfig.extra;
 
   const getAnswer = async () => {
-    setState((prev) => ({
-      ...prev,
-      isLoading: true,
-    }));
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        question: userInput,
-      }),
-    };
-    console.log(`${API_URL}/gemini/gemini-request`);
-    const response = await fetch(
-      `${API_URL}/gemini/gemini-request`,
-      requestOptions
-    );
-
-    const result = await response.json();
-
-    if (result.status) {
-      setState((prev) => {
-        prev.messages.push({
-          data: result.data,
-          sender: "gemini",
-        });
-
-        return { ...prev };
+    try {
+      setState((prev) => ({
+        ...prev,
+        isLoading: true,
+      }));
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          question: userInput,
+        }),
+      };
+      console.log(`${API_URL}/gemini/gemini-request`);
+      const response = await fetchWithTimeOut({
+        url: `${API_URL}/gemini/gemini-request`,
+        requestOptions,
       });
+
+      const result = await response.json();
+      if (result.status) {
+        setState((prev) => {
+          prev.messages.push({
+            data: result.data,
+            sender: "gemini",
+          });
+
+          return { ...prev };
+        });
+      }
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+      }));
+    } catch (error) {
+      console.log(error);
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+      }));
     }
-    setState((prev) => ({
-      ...prev,
-      isLoading: false,
-    }));
   };
   useEffect(() => {
     messageRef.current.scrollToEnd();
